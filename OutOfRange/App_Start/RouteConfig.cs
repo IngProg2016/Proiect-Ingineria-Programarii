f9ad248a3d07dd14fa8ed8be44d1bf1b95afb4ff
Dima Robert <lord.specter@hotmail.com>
2016-03-29 19:32:20 +0300

    Excluded /api/* routes from SPA.

--- OutOfRange/App_Start/RouteConfig.cs
+++ OutOfRange/App_Start/RouteConfig.cs
@@ -12,6 +12,8 @@ namespace OutOfRange
         public static void RegisterRoutes(RouteCollection routes)
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+            routes.IgnoreRoute("api/{*any}");
+
 
             routes.MapRoute(
                 name: "Default",

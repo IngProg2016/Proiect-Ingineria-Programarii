9f11eaba645f95eaf408ea49c7449514cca2acf0
Sateanu <andrei.sateanu@gmail.com>
2016-04-05 12:35:40 +0300

    [Backend]: Edmx up and question controller

---
+++ OutOfRange/Controllers/QuestionsController.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Description;
+using OutOfRange.Models;
+
+namespace OutOfRange.Controllers
+{
+    public class QuestionsController : ApiController
+    {
+        private OutOfRangeEntities db = new OutOfRangeEntities();
+
+        // GET: api/Questions
+        public IQueryable<Question> GetQuestions()
+        {
+            return db.Questions;
+        }
+
+        // GET: api/Questions/5
+        [ResponseType(typeof(Question))]
+        public IHttpActionResult GetQuestion(Guid id)
+        {
+            Question question = db.Questions.Find(id);
+            if (question == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(question);
+        }
+
+        // PUT: api/Questions/5
+        [ResponseType(typeof(void))]
+        public IHttpActionResult PutQuestion(Guid id, Question question)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != question.ID)
+            {
+                return BadRequest();
+            }
+
+            db.Entry(question).State = EntityState.Modified;
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!QuestionExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        // POST: api/Questions
+        [ResponseType(typeof(Question))]
+        public IHttpActionResult PostQuestion(Question question)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+            
+            db.Questions.Add(question);
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateException)
+            {
+                if (QuestionExists(question.ID))
+                {
+                    return Conflict();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return CreatedAtRoute("DefaultApi", new { id = question.ID }, question);
+        }
+
+        // DELETE: api/Questions/5
+        [ResponseType(typeof(Question))]
+        public IHttpActionResult DeleteQuestion(Guid id)
+        {
+            Question question = db.Questions.Find(id);
+            if (question == null)
+            {
+                return NotFound();
+            }
+
+            db.Questions.Remove(question);
+            db.SaveChanges();
+
+            return Ok(question);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool QuestionExists(Guid id)
+        {
+            return db.Questions.Count(e => e.ID == id) > 0;
+        }
+    }
+}

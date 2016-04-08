afa72f01d8428ee3fafcc828d2c6321a8f2228e4
Sateanu <andrei.sateanu@gmail.com>
2016-04-08 12:55:39 +0300

    [Backend]: Answers & Categories Controller

---
+++ OutOfRange/Controllers/AnswersController.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Description;
+using System.Web.Http.Results;
+using Microsoft.AspNet.Identity;
+using OutOfRange.Models;
+
+namespace OutOfRange.Controllers
+{
+    public class AnswersController : ApiController
+    {
+        private OutOfRangeEntities db = new OutOfRangeEntities();
+
+        // GET: api/Answers
+        public JsonResult<IEnumerable<AnswerDTO>> GetAnswers()
+        {
+            return Json(db.Answers.ToList().Select(AnswerDTO.FromEntity));
+        }
+        //GET: api/Answers/{GUID}
+        public JsonResult<IEnumerable<AnswerDTO>> GetAnswers(Guid id)
+        {
+            return Json(db.Answers.Where(x=>x.QuestionID==id).ToList().Select(AnswerDTO.FromEntity));
+        }
+
+        // GET: api/Answers/5
+        [ResponseType(typeof(AnswerDTO))]
+        public IHttpActionResult GetAnswer(Guid id)
+        {
+            Answer answer = db.Answers.Find(id);
+            if (answer == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(new AnswerDTO(answer));
+        }
+
+        // PUT: api/Answers/5
+        [ResponseType(typeof(void))]
+        public IHttpActionResult PutAnswer(Guid id, Answer answer)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != answer.ID)
+            {
+                return BadRequest();
+            }
+
+            db.Entry(answer).State = EntityState.Modified;
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!AnswerExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        // POST: api/Answers
+        [Authorize]
+        [ResponseType(typeof(AnswerDTO))]
+        public IHttpActionResult PostAnswer(Answer answer)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            answer.ID = Guid.NewGuid();
+            answer.Added=DateTime.Now;
+            answer.UserID = User.Identity.GetUserId();
+            db.Answers.Add(answer);
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateException)
+            {
+                if (AnswerExists(answer.ID))
+                {
+                    return Conflict();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+            db=new OutOfRangeEntities();
+            answer = db.Answers.Find(answer.ID);
+            return CreatedAtRoute("DefaultApi", new { id = answer.ID }, AnswerDTO.FromEntity(answer));
+        }
+
+        // DELETE: api/Answers/5
+        [ResponseType(typeof(Answer))]
+        public IHttpActionResult DeleteAnswer(Guid id)
+        {
+            Answer answer = db.Answers.Find(id);
+            if (answer == null)
+            {
+                return NotFound();
+            }
+
+            db.Answers.Remove(answer);
+            db.SaveChanges();
+
+            return Ok(answer);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool AnswerExists(Guid id)
+        {
+            return db.Answers.Count(e => e.ID == id) > 0;
+        }
+    }
+}
---
+++ OutOfRange/Controllers/CategoriesController.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Description;
+using System.Web.Http.Results;
+using OutOfRange.Models;
+
+namespace OutOfRange.Controllers
+{
+    public class CategoriesController : ApiController
+    {
+        private OutOfRangeEntities db = new OutOfRangeEntities();
+
+        // GET: api/Categories
+        public JsonResult<IEnumerable<CategoryDTO>> GetCategories()
+        {
+            return Json(db.Categories.ToList().Select(CategoryDTO.FromEntity));
+        }
+
+        // GET: api/Categories/5
+        [ResponseType(typeof(CategoryDTO))]
+        public IHttpActionResult GetCategory(Guid id)
+        {
+            Category category = db.Categories.Find(id);
+            if (category == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(new CategoryDTO(category));
+        }
+
+        // PUT: api/Categories/5
+        [ResponseType(typeof(void))]
+        public IHttpActionResult PutCategory(Guid id, Category category)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != category.ID)
+            {
+                return BadRequest();
+            }
+
+            db.Entry(category).State = EntityState.Modified;
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!CategoryExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        // POST: api/Categories
+        [ResponseType(typeof(CategoryDTO))]
+        public IHttpActionResult PostCategory(Category category)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            db.Categories.Add(category);
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateException)
+            {
+                if (CategoryExists(category.ID))
+                {
+                    return Conflict();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return CreatedAtRoute("DefaultApi", new { id = category.ID }, new CategoryDTO());
+        }
+
+        // DELETE: api/Categories/5
+        [ResponseType(typeof(Category))]
+        public IHttpActionResult DeleteCategory(Guid id)
+        {
+            Category category = db.Categories.Find(id);
+            if (category == null)
+            {
+                return NotFound();
+            }
+
+            db.Categories.Remove(category);
+            db.SaveChanges();
+
+            return Ok(category);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool CategoryExists(Guid id)
+        {
+            return db.Categories.Count(e => e.ID == id) > 0;
+        }
+    }
+}

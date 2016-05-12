cb81d81f553144672ebe073af5fcc50ec04f515b
Sateanu <andrei.sateanu@gmail.com>
2016-05-12 11:58:26 +0300

    Updated EDMX

--- OutOfRange/Models/Models.Designer.cs
+++ OutOfRange/Models/Models.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'D:\Projects\Proiect-Ingineria-Programarii\OutOfRange\Models\Models.edmx'. 
+﻿// T4 code generation is enabled for model 'D:\Coding Stuff\C#\CancerAKAIP\OutOfRange\Models\Models.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.

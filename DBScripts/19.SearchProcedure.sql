c7cd8e2e089821c8094b02d598348843e7b92c4e
Vlad-Alexandru Belu <vlad.belu@live.com>
2016-05-27 17:48:28 +0300

    Search Procedure it works... Sort of

--- DBScripts/19.SearchProcedure.sql
+++ DBScripts/19.SearchProcedure.sql
@@ -7,6 +7,8 @@ CREATE PROCEDURE Search
 AS
 
 BEGIN
+	DECLARE
+		@Terms nvarchar(512) = 'tiny'
 	DECLARE question_cursor CURSOR FOR 
 		SELECT q.ID, q.UserID, q.CategoryID, q.Title, q.QuestionBody, q.Added, q.Modified, q.Score, q.Bounty FROM Questions q;
 	DECLARE @temp_table TABLE 
@@ -32,7 +34,8 @@ BEGIN
 		@Modified datetime,
 		@Score decimal(18, 0),
 		@Bounty decimal(18, 0),
-		@Rank int
+		@Rank int,
+		@Term nvarchar(128)
 
 	OPEN question_cursor
 	FETCH NEXT FROM question_cursor 
@@ -43,24 +46,29 @@ BEGIN
 		SELECT @Rank = 0;
 
 		--1 Check in the title
-		SELECT @Rank += (COUNT(Name) * 7)  
-		FROM Quesntions q
-		WHERE q.ID = @ID AND t.Title LIKE '%' + @Terms + '%'
+		SELECT @Rank += DIFFERENCE(q.Title, @Terms) * 7 FROM Questions q WHERE q.ID = @ID 
 
 		--2 Check for tags
 		SELECT @Rank += (COUNT(Name) * 6)  
-		FROM Quesntions q
+		FROM Questions q
 			INNER JOIN QuestionTags qt ON (q.ID = qt.QuestionID)
 			INNER JOIN Tags t ON (qt.TagID = t.ID)
-		WHERE q.ID = @ID AND t.Name LIKE '% ' + @Terms + ' %'
+		WHERE q.ID = @ID AND @Terms LIKE '%' + t.Name + '%'
 
 		--3 Check Question Body
+		SELECT @Rank += DIFFERENCE(q.QuestionBody, @Terms) * 5 FROM Questions q WHERE q.ID = @ID 
 
-		--4 Check Answers
-		SELECT @Rank += (COUNT(Name) * 4)  
-		FROM Quesntions q
+		--4 Check Answers (a)
+		SELECT @Rank += (COUNT(*) * 4)  
+		FROM Questions q
 			INNER JOIN Answers a ON (q.ID = a.QuestionID)
-		WHERE q.ID = @ID AND a.AnswerBody LIKE '%' + @Terms + '%'
+		WHERE q.ID = @ID AND a.Accepted = 1 AND a.AnswerBody LIKE '%' + @Terms + '%'
+
+		--4 Check Answers (b)
+		SELECT @Rank += (COUNT(*) * 3)  
+		FROM Questions q
+			INNER JOIN Answers a ON (q.ID = a.QuestionID)
+		WHERE q.ID = @ID AND a.Accepted <> 1 AND a.AnswerBody LIKE '%' + @Terms + '%'
 
 		--5 Check Users
 
@@ -69,9 +77,12 @@ BEGIN
 		IF @Rank > 0
 			INSERT INTO @temp_table(ID, UserID, CategoryID, Title, QuestionBody, Added, Modified, Score, Bounty, [Rank])
 			VALUES(@ID, @UserID, @CategoryID, @Title, @QuestionBody, @Added, @Modified, @Score, @Bounty, @Rank)
+
+		FETCH NEXT FROM question_cursor 
+		INTO @ID, @UserID, @CategoryID, @Title, @QuestionBody, @Added, @Modified, @Score, @Bounty
 	END
 	CLOSE question_cursor;
 	DEALLOCATE question_cursor;
 
-	SELECT ID, UserID, CategoryID, Title, QuestionBody, Added, Modified, Score, Bounty FROM @temp_table ORDER BY Rank DESC
+	SELECT ID, UserID, CategoryID, Title, QuestionBody, Added, Modified, Score, Bounty, [Rank] FROM @temp_table ORDER BY Rank DESC
 END
